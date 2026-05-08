b71c99a2f28190c7711e81a546e45fefd89fb8c0
Jonas <jonascanaza@gmail.com>
2026-05-07 23:21:58 -0300

    Add player scores to GameManager

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -10,6 +10,10 @@ public class BlockController : MonoBehaviour
 
     [Header("Block Settings")]
     [SerializeField] private int scoreValue = 20;
+    [SerializeField] private float perfectOverlap = 90.0f;
+    [SerializeField] private float goodOverlap = 45.0f;
+    private const float GRAVITY_MULTIPLIER_OFFSET = 1.0f;
+    private const float MAX_PERCENTAGE = 100.0f;
 
     private void Awake()
     {
@@ -30,13 +34,22 @@ public class BlockController : MonoBehaviour
         if (!isPlaced)
         {
             PlaceBlock(collision);
-            GameManager.Instance.AddScore(scoreValue);
+
+            if (GameManager.Instance.BlocksPlaced == 0)
+            {
+                GameManager.Instance.AddScore(scoreValue);
+                GameManager.Instance.RegisterBlock();
+            }
+            else
+            {
+                EvaluatePlacement(collision);
+            }
         }
     }
 
     private void ApplyExtraGravity()
     {
-        rb.AddForce(Physics.gravity * (gravityMultiplier - 1.0f), ForceMode.Acceleration);
+        rb.AddForce(Physics.gravity * (gravityMultiplier - GRAVITY_MULTIPLIER_OFFSET), ForceMode.Acceleration);
     }
 
     private void PlaceBlock(Collision collision)
@@ -56,12 +69,55 @@ public class BlockController : MonoBehaviour
 
     private void AlignOnTop(Collision collision)
     {
-        float height = col.bounds.size.y;
+        float thisHeight = col.bounds.size.y;
         float otherTop = collision.collider.bounds.max.y;
 
         Vector3 newPosition = transform.position;
-        newPosition.y = otherTop + height / 2.0f;
+        newPosition.y = otherTop + thisHeight / 2.0f;
 
         transform.position = newPosition;
+    }
+
+    private void EvaluatePlacement(Collision collision)
+    {
+        float overlapPercentage = GetOverlapPercentage(collision);
+        bool isValidPlacement = true;
+
+        if (overlapPercentage > perfectOverlap)
+        {
+            GameManager.Instance.AddScore(scoreValue * 2);
+            GameManager.Instance.AddPerfectPlacement();
+        }
+        else if (overlapPercentage > goodOverlap)
+        {
+            GameManager.Instance.AddScore(scoreValue);
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.FreezePositionZ;
+            rb.isKinematic = false;
+            isValidPlacement = false;
+        }
+
+        if (isValidPlacement)
+        {
+            GameManager.Instance.RegisterBlock();
+        }
+    }
+
+    private float GetOverlapPercentage(Collision collision)
+    {
+        float thisLeftEdge = col.bounds.min.x;
+        float thisRightEdge = col.bounds.max.x;
+        float otherLeftEdge = collision.collider.bounds.min.x;
+        float otherRightEdge = collision.collider.bounds.max.x;
+
+        float overlapMin = Mathf.Max(thisLeftEdge, otherLeftEdge);
+        float overlapMax = Mathf.Min(thisRightEdge, otherRightEdge);
+        float overlap = Mathf.Max(0.0f, overlapMax - overlapMin);
+
+        float blockWidth = col.bounds.size.x;
+
+        return Mathf.Clamp((overlap / blockWidth) * MAX_PERCENTAGE, 0.0f, MAX_PERCENTAGE);
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,12 +1,21 @@
-using UnityEngine;
-
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
     public int Score { get; private set; }
+    public int PerfectPlacements { get; private set; }
+    public int BlocksPlaced { get; private set; }
 
     public void AddScore(int points)
     {
         Score += points;
-        Debug.Log("Puntos: " + Score);
+    }
+
+    public void AddPerfectPlacement()
+    {
+        PerfectPlacements++;
+    }
+
+    public void RegisterBlock()
+    {
+        BlocksPlaced++;
     }
 }
